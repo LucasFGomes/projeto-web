e963b6c09033e6b1f37c029de832407e5c4fe5f6
Lucas Ferreira <lucasgomm33@gmail.com>
2018-10-15 21:27:57 -0300

    siz commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var inputs = document.querySelectorAll('input[type="checkbox"]');
 
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('click', sumSubtractValor);
+  inputs[i].addEventListener('click', somaSubtraiValor);
 }
 
-function sumSubtractValor() {
+function somaSubtraiValor() {
   var resultado = parseFloat(this.value);
   var principal = document.getElementById('total-pagar');
   var total = parseFloat(principal.value);
@@ -12,4 +12,15 @@ function sumSubtractValor() {
   this.checked == true ? total += resultado : total -= resultado;
 
   principal.value = total;
+}
+
+function agendar() {
+  var inputs = document.querySelectorAll('input[type="checkbox"]');
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      return alert('Agendado com sucesso!');
+    }
+  }
+  return alert('Falha ao agendar, precisa marcar um dos serviços!');
 }
